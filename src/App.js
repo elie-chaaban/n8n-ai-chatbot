3095e0c2e7990a12c96ba14e2feddd0b0382d6be
Elie Chaaban <elie_chaaban@hotmail.com>
2025-06-04 12:10:00 +0300

    Add @n8n/chat dependency and implement chat functionality in App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import "@n8n/chat/style.css";
+import { createChat } from "@n8n/chat";
+import { useEffect } from "react";
+
+export const App = () => {
+  useEffect(() => {
+    createChat({
+      webhookUrl:
+        "https://test-elie-chaaban.app.n8n.cloud/webhook/f172d164-27f6-4afb-9d2a-3ffd1797b552/chat",
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -20,6 +30,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
